83b890e796b7e584940815b345c19a896f824236
guix <guillaume.duveau@gmail.com>
2017-12-04 22:48:39 +0100

    fix JS error if no image

--- js/gd-header.js
+++ js/gd-header.js
@@ -6,13 +6,14 @@
 (function ($, Drupal) {
   Drupal.behaviors.gd_header = {
     attach: function (context, settings) {
-      if (drupalSettings.gd_header.block_img_url !== undefined) {
-        url = drupalSettings.gd_header.block_img_url;
-      }
       if (drupalSettings.gd_header.node_img_url !== undefined) {
         url = drupalSettings.gd_header.node_img_url;
+        $('#block-gdheader').css('background-image', 'url(' + url + ')');
       }
-      $('#block-gdheader').css('background-image', 'url(' + url + ')');
+      else if (drupalSettings.gd_header.block_img_url !== undefined) {
+        url = drupalSettings.gd_header.block_img_url;
+        $('#block-gdheader').css('background-image', 'url(' + url + ')');
+      }
     }
   };
 })(jQuery, Drupal);
